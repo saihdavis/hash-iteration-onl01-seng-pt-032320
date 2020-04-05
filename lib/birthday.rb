26dd3f396f91beb47eaab8019c117d5349655132
Saida Davis <saihdavis@gmail.com>
2020-04-05 17:29:46 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,7 +15,7 @@ def age_appropriate_birthday(birthday_kids)
     if age < 12
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     else
-      false
+      "You are too old for this."
   end
 end
 end
